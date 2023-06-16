f8e6b0fc215597f7b5702265394902474969b1b4
Demetrio Cruz <hesenrre@gmail.com>
2023-06-15 18:42:13 -0600

    controller test added

--- test/controllers/weather_controller_test.rb
+++ test/controllers/weather_controller_test.rb
@@ -1,8 +1,20 @@
 require "test_helper"
 
 class WeatherControllerTest < ActionDispatch::IntegrationTest
+
   test "should get index" do
-    get weather_index_url
+    get root_url
     assert_response :success
   end
+
+  test "should not search over HTTP plain method" do
+    post weather_search_url, params: {query: "95129"}
+    assert_response :not_found
+  end
+
+  test "should search over xhr" do
+    post weather_search_url, params: {query: "95129"}, xhr: true
+    assert_response :success
+  end
+
 end
